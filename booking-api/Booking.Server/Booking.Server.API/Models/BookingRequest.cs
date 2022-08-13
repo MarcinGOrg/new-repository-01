e0de8b9cc4c6ecab0fd29f5e4f6402bb418df51c
kenicram <98278551+kenicram@users.noreply.github.com>
2022-08-13 14:17:03 +0200

    Delete booking-api/Booking.Server directory

--- booking-api/Booking.Server/Booking.Server.API/Models/BookingRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Booking.Server.API.Models
-{
-    public class BookingRequest
-    {
-        public Guid CarId { get; set; }
-        public DateTime DateFrom { get; set; }
-        public DateTime DateTo { get; set; }
-    }
-}
